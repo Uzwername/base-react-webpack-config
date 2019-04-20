48242a8c0924f7c74a293c85be9169aec00628c6
Uzwername <QQwerty7589@gmail.com>
2019-04-20 23:48:37 +0200

    Fragment test folder added
    A sample test added in order to preserve Fragments folder

---
+++ __tests__/Fragment/fragment.test.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { shallow, render } from "enzyme";
+// import {A} from "...";
+
+describe (`Component <A /> & component <B /> interaction`, () => {
+
+	test(`A test that shows that <A /> &
+		<B /> play well together`, () => {
+		
+		//expect
+	
+	});
+
+});
