00623d667bd44119222b08d100443d56d24976d0
IT-Suitapest <it@suitaprest.com>
2019-07-16 11:10:50 +0200

    babel config improved

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,19 @@
-module.exports = {
-  presets: ["@babel/preset-env", "@babel/preset-react"],
-  plugins: ["transform-remove-console"]
-};
+module.exports = (function () {
+
+	if (process.env.NODE_ENV === `production`) {
+
+		// No console
+		return {
+			presets: ["@babel/preset-env", "@babel/preset-react"],
+  			plugins: ["transform-remove-console"]
+		};
+
+	} else {
+
+		return {
+			presets: ["@babel/preset-env", "@babel/preset-react"]
+		};	
+
+	}
+
+})();
